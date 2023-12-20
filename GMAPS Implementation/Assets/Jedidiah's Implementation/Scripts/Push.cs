d32818a4d9bed46a5b6fe6fb259fb04d0a0f0c84
jedidia60 <jedidiahwnt@hotmail.com>
2023-12-20 18:18:53 +0800

    Jedidiah's implementation
    Added comments

--- GMAPS Implementation/Assets/Jedidiah's Implementation/Scripts/Push.cs
+++ GMAPS Implementation/Assets/Jedidiah's Implementation/Scripts/Push.cs
@@ -6,6 +6,7 @@ public class Push : MonoBehaviour
 {
     Camera cam;
 
+    // Place this script into the camera because the mouse position is relative to the camera position
     private void Start()
     {
         cam = GetComponent<Camera>();
@@ -13,15 +14,28 @@ public class Push : MonoBehaviour
 
     private void Update()
     {
+        // Check if left click is being held
         if (Input.GetMouseButton(0))
         {
+            // Create a ray and check if the raycast hits any object
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
+                // If it hits an object then modify it's transform
                 if (hit.collider != null)
                 {
+                    Debug.DrawRay(ray.origin, ray.direction * 100f, Color.red);
+
+                    // Get which part did the raycast hit in relative to the object's origin
                     Vector3 direction = (hit.transform.position - hit.point).normalized;
-                    hit.rigidbody.AddTorque(new Vector3(-direction.y * hit.transform.localScale.y * 2.5f, direction.x * hit.transform.localScale.x * 5f, 0f), ForceMode.Force);
+
+                    // Spin the object using AddTorque
+                    // Use object scale as the larger the object the further the mouse needs to be from the center of origin to achieve the same effect as a small object
+                    // Flip the x and y component of the hit point to the Vector3 for AddTorque, as the horizontal rotation is on the Y axis and the vertical rotation is on the X axis
+                    hit.rigidbody.AddTorque(new Vector3(-direction.y * hit.transform.localScale.y * 2.5f, direction.x * hit.transform.localScale.x * 10f, 0f), ForceMode.Force);
+
+                    // Push the object in the direction of the hit point in relative to the object's origin
+                    // So if the left side is clicked it would move slightly to the right
                     hit.rigidbody.AddForce(direction * 10f, ForceMode.Force);
                 }
             }
